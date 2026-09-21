547206d7b41c7c3163ef10c3f3ca6002a03c7092
FakkuDev <fjgonzalez0000@gmail.com>
2026-09-20 22:14:14 -0300

    Fix 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://jg-cuchillos.pages.dev',
   output: 'static',
-  integrations: [
-    preact(),
-    tailwind(),
-    sitemap({
-      filter: (page) => !page.includes('/admin/')
-    })
-  ]
+  integrations: [preact(), tailwind()]
 });
