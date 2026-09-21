3c521ca14f7a272fb433d86958d816078323fd63
FakkuDev <fjgonzalez0000@gmail.com>
2026-09-20 22:07:29 -0300

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,13 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://jg-cuchillos.pages.dev',
+  output: 'static',
   integrations: [
     preact(),
     tailwind(),
     sitemap({
-      // Excluimos el panel de admin del sitemap
       filter: (page) => !page.includes('/admin/')
     })
-  ],
-  output: 'static',
-  // Usá la URL que te dio Cloudflare (ej: https://jg-cuchillos.pages.dev)
-  // o tu dominio propio cuando lo tengas.
-  site: 'https://jg-cuchillos.pages.dev',
+  ]
 });
